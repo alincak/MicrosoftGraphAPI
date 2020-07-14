6b2c98ee1a69dfb316b9965cca083a8747025735
kerem <kerem@phonoclick.com>
2020-07-14 10:38:21 +0300

    first commit

---
+++ MicrosoftGraphAPIMessage/Integrations/MicrosoftGraph.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Integrations
+{
+  public class MicrosoftGraph
+  {
+    private readonly GraphServiceClient graphClient;
+
+    public MicrosoftGraph(string token)
+    {
+      graphClient = CreateGraphClientAsync(token);
+    }
+
+    private GraphServiceClient CreateGraphClientAsync(string token)
+    {
+      return new GraphServiceClient(
+             new DelegateAuthenticationProvider(
+                (request) =>
+                {
+                  if (!string.IsNullOrEmpty(token))
+                  {
+                    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                  }
+                  return Task.FromResult(0);
+                }));
+    }
+
+    public async Task<string> CreateDraftMessage(string messageId, IList<string> toEmails, string subject, string body)
+    {
+      if (toEmails == null || !toEmails.Any() || string.IsNullOrEmpty(subject) || string.IsNullOrEmpty(body))
+      {
+        return null;
+      }
+
+      var message = new Message
+      {
+        Subject = subject,
+        Body = new ItemBody
+        {
+          ContentType = BodyType.Html,
+          Content = body
+        },
+        ToRecipients = toEmails.Select(x => new Recipient { EmailAddress = new EmailAddress { Address = x } })
+      };
+
+      try
+      {
+        Message resultMessage = null;
+        if (string.IsNullOrEmpty(messageId))
+        {
+          resultMessage = await graphClient.Me.Messages
+                                .Request()
+                                .AddAsync(message);
+        }
+        else
+        {
+          resultMessage = await graphClient.Me.Messages[messageId]
+                                 .CreateReply(message)
+                                 .Request()
+                                 .PostAsync();
+        }
+
+        return resultMessage?.Id;
+      }
+      catch (Exception ex)
+      {
+        //Logger.ErrorFormat("CreateDraftMessage - {0}", ex);
+        return null;
+      }
+    }
+
+    public async Task<bool> AddAttachmentToMessage(string messageId, string fileName, byte[] bytes)
+    {
+      try
+      {
+        var result = true;
+        if (bytes.Length >= 3000000)
+        {
+          result = await MessageCreateUploadSession(messageId, fileName, bytes);
+        }
+        else
+        {
+          var attachment = new FileAttachment
+          {
+            Name = fileName,
+            ContentBytes = bytes
+          };
+
+          await graphClient.Me.Messages[messageId].Attachments
+                      .Request()
+                      .AddAsync(attachment).ConfigureAwait(false);
+        }
+
+        if (!result) throw new Exception("Upload failed.");
+
+        return result;
+      }
+      catch
+      {
+        await DeleteMessage(messageId);
+        return false;
+      }
+    }
+
+    private async Task<bool> MessageCreateUploadSession(string messageId, string fileName, byte[] bytes)
+    {
+      try
+      {
+        using (Stream stream = new MemoryStream(bytes))
+        {
+          var attachmentItem = new AttachmentItem
+          {
+            AttachmentType = AttachmentType.File,
+            Name = fileName,
+            Size = stream.Length
+          };
+
+          var uploadSession = await graphClient.Me.Messages[messageId].Attachments
+                                    .CreateUploadSession(attachmentItem)
+                                    .Request()
+                                    .PostAsync();
+
+          var maxChunkSize = 320 * 1024;
+          var largeFileUploadTask = new LargeFileUploadTask<FileAttachment>(uploadSession, stream, maxChunkSize);
+
+          var uploadResult = await largeFileUploadTask.UploadAsync();
+          if (uploadResult.UploadSucceeded)
+          {
+            return true;
+          }
+
+          return false;
+        }
+      }
+      catch (Exception ex)
+      {
+        //Logger.ErrorFormat("CreateUploadSession - Upload failed ex: {0}", ex);
+        return false;
+      }
+    }
+
+    public async Task<(bool success, string message)> SendMessage(string messageId)
+    {
+      try
+      {
+        await graphClient.Me.Messages[messageId]
+                    .Send()
+                    .Request()
+                    .PostAsync();
+
+        return (true, null);
+      }
+      catch (Exception ex)
+      {
+        //Logger.ErrorFormat("SendMessage - {0}", ex);
+        await DeleteMessage(messageId);
+        return (false, ex.Message);
+      }
+    }
+
+    private async Task DeleteMessage(string messageId)
+    {
+      try
+      {
+        await graphClient.Me.Messages[messageId]
+              .Request()
+              .DeleteAsync();
+      }
+      catch (Exception ex)
+      {
+        //Logger.ErrorFormat("DeleteMessage - ex: {0}", ex);
+      }
+    }
+
+  }
+}
---
+++ MicrosoftGraphAPIMessage/MicrosoftGraphAPIMessage/Controllers/HomeController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Integrations;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MicrosoftGraphAPIMessage.Models;
+
+namespace MicrosoftGraphAPIMessage.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+      _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> SendMail()
+    {
+      var files = Request.Form.Files;
+
+      var _token = "";
+      var microsoftGraph = new MicrosoftGraph(_token);
+
+      var toEmails = new List<string> { "email@domain.com" };
+      var messageId = await microsoftGraph.CreateDraftMessage(null, toEmails, "Medium test", "medium body");
+      if (!string.IsNullOrEmpty(messageId))
+      {
+        var attachmentsResult = await AddAttachmentToOffice365Message(microsoftGraph, files, messageId);
+        if (!attachmentsResult)
+        {
+          return Content("Failed.");
+        }
+
+        var sendResult = await microsoftGraph.SendMessage(messageId);
+        if (!sendResult.success)
+        {
+          return Content("Failed.");
+        }
+      }
+
+      return Content("OK.");
+    }
+
+    //Aslında burası service içerisinde ama burada öyle bir yapı olmadığından buraya ekledim.
+    private async Task<bool> AddAttachmentToOffice365Message(MicrosoftGraph microsoftGraph, IFormFileCollection files, string messageId)
+    {
+      if (microsoftGraph == null || files == null)
+      {
+        return false;
+      }
+
+      foreach (var file in files)
+      {
+        var result = await microsoftGraph.AddAttachmentToMessage(messageId, file.FileName, IFormFileToArray(file));
+        if (!result)
+        {
+          return false;
+        }
+      }
+
+      return true;
+    }
+
+    //Aslında burasıda Helper içerisinde
+    private byte[] IFormFileToArray(IFormFile file)
+    {
+      using (var memoryStream = new MemoryStream())
+      {
+        file.CopyTo(memoryStream);
+        return memoryStream.ToArray();
+      }
+    }
+
+  }
+}
